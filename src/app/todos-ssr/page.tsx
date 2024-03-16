244bc27ad8fd2147c5dfdd5dd946d6f708b0024d
YEONJAE <121925181+porosadporosad@users.noreply.github.com>
2024-03-16 19:04:59 +0900

    Update page.tsx

--- src/app/todos-ssr/page.tsx
+++ src/app/todos-ssr/page.tsx
@@ -2,13 +2,13 @@ import { TodoType } from '@/types';
 import React from 'react'
 
 async function SSR() {
-  const response = await fetch("http://localhost:3000/api/todos", {
+  const response = await fetch("http://localhost:4000/todos", {
     cache: "no-cache",
   });
-  const  {todos}:{todos:TodoType[]}  = await response.json();
+  const todos  = await response.json();
   return (
     <div className='flex flex-col justify-center items-center'>
-      {todos.map((prev)=> {
+      {todos.map((prev:TodoType)=> {
         return(<div key={prev.id} className='bg-blue-100 border border-blue-400 text-blue-700 p-8 m-2 rounded w-72'>
            <div>{prev.title}</div>
             <div>{prev.contents}</div>
